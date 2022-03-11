dc0e0b1bc448aaabaa233aef6d03b9a6ceeabeb1
Jakub Wąsala <jakubwasala1@gmail.com>
2022-03-11 17:28:04 +0100

    created GA class stub

---
+++ si_project/flo/genetic_algorithm.py
@@ -0,0 +1,12 @@
+class GeneticAlgorithm:
+    def __init__(self, population_size: int):
+        """
+        Constructor generates initial random population.
+        """
+        pass
+
+    def create_new_generation(self) -> None:
+        """
+        Replaces current population with new one by using GA operators.
+        """
+        pass
