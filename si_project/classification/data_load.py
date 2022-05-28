5bc89c6afc2627eaa58c78e48b37ecb2f68c2637
Jakub Wąsala <jakubwasala1@gmail.com>
2022-05-28 10:38:40 +0200

    created data loading function

---
+++ si_project/classification/data_load.py
@@ -0,0 +1,49 @@
+import json
+from logging import getLogger
+
+from .model import BookRaw
+
+logger = getLogger(__name__)
+
+
+def load_books(stream) -> list[BookRaw]:
+    books: list[BookRaw] = []
+    exception_count = 0
+
+    for line_i, line in enumerate(stream):
+        # Split line into columns
+        try:
+            article_id_raw, freebase_id_raw, title, author, pub_date_raw, genres, plot = line.split('\t')
+        except ValueError as ve:
+            logger.warning(f'Couldn\'t unpack line {line_i}, values "{line}": {ve}')
+            exception_count += 1
+            continue
+        # Parse integers
+        try:
+            article_id = int(article_id_raw) if article_id_raw else None
+            freebase_id = int(freebase_id_raw) if freebase_id_raw else None
+        except ValueError as ve:
+            logger.warning(f'Couldn\'t parse integer value in line {line_i}, values "{line}": {ve}')
+            exception_count += 1
+            continue
+        # Parse date
+        try:
+            pub_year = int(pub_date_raw.split('-')[0]) if pub_date_raw else None
+        except ValueError as ve:
+            logger.warning(f'Couldn\'t parse year of publication in line {line_i}, values "{line}": {ve}')
+            exception_count += 1
+            continue
+        # Parse genres
+        try:
+            if genres:
+                genres = json.loads(genres).values()
+            else:
+                genres = []
+        except Exception as e:
+            logger.warning(f'Couldn\'t parse genres in line {line_i}, values "{line}": {e}')
+            exception_count += 1
+            continue
+        books.append(BookRaw(article_id, freebase_id, title, author, pub_year, genres, plot))
+
+    logger.info(f'Parsed {len(books)} books, {exception_count} were ignored due to parsing errors.')
+    return books
