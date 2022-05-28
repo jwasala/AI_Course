d95bf150a98370e4caa8e00e8bf5ffa23bb05db3
Jakub Wąsala <jakubwasala1@gmail.com>
2022-05-28 10:38:32 +0200

    simplified names of model attributes and replaced publication_date with publication_year

--- si_project/classification/model.py
+++ si_project/classification/model.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from datetime import date
 
 
 @dataclass
@@ -8,14 +7,14 @@ class BookRaw:
     Represents book data as in source file with very basic transformations:
     - article_id, freebase_id to int
     - book genres to list of strings (dict values)
-    - publication_date to datetime
+    - publication_date to publication_year (trunkate and parse int)
     """
     article_id: int | None
     freebase_id: int | None
-    book_title: str | None
+    title: str | None
     author: str | None
-    publication_date: date | None
-    book_genres: list[str]
+    publication_year: int | None
+    genres: list[str]
     plot_summary: str | None
 
 
@@ -23,12 +22,13 @@ class BookRaw:
 class Book:
     """
     Represents transformed book data:
-    - book_genres replaced with arbitrary label book_genre
+    - genres replaced with arbitrary label genre
     - values of other fields are non nullable
     """
     article_id: int
     freebase_id: int
-    book_title: str
+    title: str
     author: str
-    book_genre: str
+    publication_year: int
+    genre: str
     plot_summary: str
