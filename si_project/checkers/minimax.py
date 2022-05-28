ee00392929f5fd30a5b24c43b6869d632f0ab51e
Jakub Wąsala <jakubwasala1@gmail.com>
2022-05-28 09:55:02 +0200

    added extra null check for return value in minimax

--- si_project/checkers/minimax.py
+++ si_project/checkers/minimax.py
@@ -8,7 +8,15 @@ def minimax(side: Side, board: Board, depth=4, rating_heuristic=None) -> int:
     if abs(rating_heuristic(board)) == 1000 or depth == 0:
         return rating_heuristic(board)
     next_boards = (board.move(move) for move in board.get_possible_moves_of_side(side))
+    if not next_boards:
+        return rating_heuristic(board)
     if side == Side.White:
-        return max([minimax(Side.Black, board, depth - 1, rating_heuristic) for board in next_boards])
+        res = [minimax(Side.Black, board, depth - 1, rating_heuristic) for board in next_boards]
+        if not res:
+            return rating_heuristic(board)
+        return max(res)
     else:
-        return min([minimax(Side.White, board, depth - 1, rating_heuristic) for board in next_boards])
+        res = [minimax(Side.White, board, depth - 1, rating_heuristic) for board in next_boards]
+        if not res:
+            return rating_heuristic(board)
+        return min(res)
